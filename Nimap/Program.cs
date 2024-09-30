6f83fa30362212bf21cba3dc6623d2bf7102085d
asifansari5787 <ansarisasif828@gmail.com>
2024-10-01 00:27:13 +0530

    Changes in Routing

--- Nimap/Program.cs
+++ Nimap/Program.cs
@@ -70,7 +70,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Product}/{action=Index}/{id?}");
 
 app.MapHealthChecks("/health");
 
